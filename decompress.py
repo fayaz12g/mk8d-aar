bf1e4d553618b4703630dc45dc5c535295407ebd
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-12-24 23:31:42 -0500

    Update decompress.py

--- decompress.py
+++ decompress.py
@@ -15,9 +15,6 @@ def extract_sarc(file):
 
     name = os.path.splitext(file)[0]
     ext = SarcLib.guessFileExt(inb)
-    
-    # if ext == ".szs":
-    #     inb = DecompressYaz(inb)
 
     if ext != ".sarc":
         with open(''.join([name, ext]), "wb") as out:
@@ -68,56 +65,57 @@ def extract_szs(file):
     with open(file, "rb") as inf:
         inb = inf.read()
 
-    inb = libyaz0.decompress(inb)
+    while libyaz0.IsYazCompressed(inb):
+        inb = libyaz0.decompress(inb)
 
     name = os.path.splitext(file)[0]
     ext = SarcLib.guessFileExt(inb)
-    
-    inb = DecompressYaz(inb)
 
-    # if ext != ".sarc":
-    #     with open(''.join([name, ext]), "wb") as out:
-    #         out.write(inb)
+    if ext != ".sarc":
+        with open(''.join([name, ext]), "wb") as out:
+            out.write(inb)
 
-    arc = SarcLib.SARC_Archive()
-    arc.load(inb)
+    else:
+        arc = SarcLib.SARC_Archive()
+        arc.load(inb)
 
-    root = os.path.join(os.path.dirname(file), name)
-    if not os.path.isdir(root):
-        os.mkdir(root)
+        root = os.path.join(os.path.dirname(file), name)
+        if not os.path.isdir(root):
+            os.mkdir(root)
 
-    files = []
+        files = []
 
-    def getAbsPath(folder, path):
-        nonlocal root
-        nonlocal files
+        def getAbsPath(folder, path):
+            nonlocal root
+            nonlocal files
 
-        for checkObj in folder.contents:
+            for checkObj in folder.contents:
+                if isinstance(checkObj, SarcLib.File):
+                    files.append(["/".join([path, checkObj.name]), checkObj.data])
+
+                else:
+                    path_ = os.path.join(root, "/".join([path, checkObj.name]))
+                    if not os.path.isdir(path_):
+                        os.mkdir(path_)
+
+                    getAbsPath(checkObj, "/".join([path, checkObj.name]))
+
+        for checkObj in arc.contents:
             if isinstance(checkObj, SarcLib.File):
-                files.append(["/".join([path, checkObj.name]), checkObj.data])
+                files.append([checkObj.name, checkObj.data])
 
             else:
-                path_ = os.path.join(root, "/".join([path, checkObj.name]))
-                if not os.path.isdir(path_):
-                    os.mkdir(path_)
+                path = os.path.join(root, checkObj.name)
+                if not os.path.isdir(path):
+                    os.mkdir(path)
 
-                getAbsPath(checkObj, "/".join([path, checkObj.name]))
+                getAbsPath(checkObj, checkObj.name)
 
-    for checkObj in arc.contents:
-        if isinstance(checkObj, SarcLib.File):
-            files.append([checkObj.name, checkObj.data])
-
-        else:
-            path = os.path.join(root, checkObj.name)
-            if not os.path.isdir(path):
-                os.mkdir(path)
-
-            getAbsPath(checkObj, checkObj.name)
-
-    for file, fileData in files:
-        print(file)
-        with open(os.path.join(root, file), "wb") as out:
-            out.write(fileData)
+        for file, fileData in files:
+            print(file)
+            with open(os.path.join(root, file), "wb") as out:
+                out.write(fileData)
+    
 
 def DecompressYaz(src):
     src_end = len(src)
